4e4b2dab1774cb5fb7d7e05435eab3dd83112620
Loporrit <141286461+loporrit@users.noreply.github.com>
2025-06-28 13:35:26 +0000

    Add createWithIdentV2 (.well-known stapling)

--- MareSynchronosAPI/Routes/MareAuth.cs
+++ MareSynchronosAPI/Routes/MareAuth.cs
@@ -2,10 +2,11 @@
 
 public class MareAuth
 {
-
     public const string Auth = "/auth";
     public const string Auth_CreateIdent = "createWithIdent";
+    public const string Auth_CreateIdentV2 = "createWithIdentV2";
     public const string Auth_Register = "registerNewKey";
     public static Uri AuthFullPath(Uri baseUri) => new Uri(baseUri, Auth + "/" + Auth_CreateIdent);
+    public static Uri AuthV2FullPath(Uri baseUri) => new Uri(baseUri, Auth + "/" + Auth_CreateIdentV2);
     public static Uri AuthRegisterFullPath(Uri baseUri) => new Uri(baseUri, Auth + "/" + Auth_Register);
 }
